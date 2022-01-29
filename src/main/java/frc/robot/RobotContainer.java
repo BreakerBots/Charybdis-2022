c582a8ef4e2d3e87970d88a31ed2396dfd0c1086
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2022-01-29 14:40:51 -0800

    more intake stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,8 +8,10 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.DriveWithJoystick;
+import frc.robot.commands.IntakeToggle;
 import frc.robot.commands.MoveStraight;
 import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.Intake;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
@@ -22,6 +24,7 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final Drive driveTrain = new Drive();
+  private final Intake IntakeSys = new Intake();
   public static XboxController xbox = new XboxController(0);
   // private Joystick joystick1 = new Joystick(Constants.XBOX_PORT);
 
@@ -43,7 +46,8 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    new JoystickButton(xbox, 1).whenPressed(new MoveStraight(driveTrain, 40), false);
+    new JoystickButton(xbox, 2).whenPressed(new MoveStraight(driveTrain, 40), true);
+    new JoystickButton(xbox, 1).whenPressed(new IntakeToggle(IntakeSys));
   }
 
   /**
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,27 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.fasterxml.jackson.databind.ser.impl.IndexedStringListSerializer;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class Intake extends SubsystemBase {
-    private WPI_TalonSRX armMotor;
+    public boolean intakeState;
     private WPI_TalonSRX indexerL;
     private WPI_TalonSRX indexerR;
-    private static DoubleSolenoid pistonL;
-    private static DoubleSolenoid pistonR;
+    private WPI_TalonSRX armMTalonSRX;
+    private DoubleSolenoid intakeSol_L;
+    private DoubleSolenoid intakeSol_R;
+    private static WPI_TalonSRX intakeMain;
 
+    /** Creates a new Intake. */
     public Intake() {
-        armMotor = new WPI_TalonSRX(Constants.ARM_MOTOR_ID);
-        indexerL = new WPI_TalonSRX(Constants.L_INDEX_ID);
-        indexerR = new WPI_TalonSRX(Constants.R_INDEX_ID);
+        intakeMain = new WPI_TalonSRX(Constants.INTAKEMAIN_ID);
+        indexerL = new WPI_TalonSRX(Constants.INTAKE_L_ID);
+        indexerR = new WPI_TalonSRX(Constants.INTAKE_R_ID);
+        intakeSol_L = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 
+                Constants.L_INTAKESOL_FWD, Constants.L_INTAKESOL_REV);
+        intakeSol_R = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,
+                Constants.R_INTAKESOL_FWD, Constants.R_INTAKESOL_REV);
+    }
 
-        pistonL = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,
-        Constants.L_INTAKE_FWD, Constants.L_INTAKE_REV);
-        pistonR = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,
-        Constants.R_INTAKE_FWD, Constants.R_INTAKE_REV);
+    public boolean intakeOnMethod() {
+        intakeSol_L.set(Value.kForward);
+        intakeSol_R.set(Value.kForward);
+        intakeMain.set(Constants.INTAKESPEED);
+        indexerL.set(Constants.L_SORTESPEED);
+        indexerR.set(Constants.R_SORTESPEED);
+        return intakeState = true;
+    }
 
+    public boolean intakeOffMethod() {
+        intakeSol_L.set(Value.kReverse);
+        intakeSol_R.set(Value.kReverse);
+        intakeMain.set(0);
+        indexerL.set(0);
+        indexerR.set(0);
+        return intakeState = false;
+    }
+
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
     }
 }
