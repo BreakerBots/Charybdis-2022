a77748558fcaf08b7b6c92968efe5c7abcb18df7
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2022-01-29 13:49:20 -0800

    Update Intake.java

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,20 +1,27 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import com.ctre.phoenix.motor;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class Intake extends SubsystemBase {
     private WPI_TalonSRX armMotor;
     private WPI_TalonSRX indexerL;
     private WPI_TalonSRX indexerR;
-    private static DoubleSolenoid intakeSol_L = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,
-            Constants.L_INTAKESOL_FWD, Constants.L_INTAKESOL_REV);
-    private static DoubleSolenoid intakeSol_R = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,
-            Constants.R_INTAKESOL_FWD, Constants.R_INTAKESOL_REV);
+    private static DoubleSolenoid pistonL;
+    private static DoubleSolenoid pistonR;
 
     public Intake() {
-        armMotor = new WPI_TalonSRX();
+        armMotor = new WPI_TalonSRX(Constants.ARM_MOTOR_ID);
+        indexerL = new WPI_TalonSRX(Constants.L_INDEX_ID);
+        indexerR = new WPI_TalonSRX(Constants.R_INDEX_ID);
+
+        pistonL = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,
+        Constants.L_INTAKE_FWD, Constants.L_INTAKE_REV);
+        pistonR = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,
+        Constants.R_INTAKE_FWD, Constants.R_INTAKE_REV);
+
     }
 }
