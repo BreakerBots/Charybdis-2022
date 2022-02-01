a2a902b418cce5fc00fa0d673b7a1adcf28b7ad0
BreakerBots5104 <79383729+BreakerBots5104@users.noreply.github.com>
2022-01-31 17:04:54 -0800

    finished charge flywheel

--- src/main/java/frc/robot/commands/ChargeFlywheel.java
+++ src/main/java/frc/robot/commands/ChargeFlywheel.java
@@ -5,20 +5,31 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Shooter;
 
 public class ChargeFlywheel extends CommandBase {
-  /** Creates a new ChargeFlywheel. */
-  public ChargeFlywheel() {
-    // Use addRequirements() here to declare subsystem dependencies.
+  Shooter shooter;
+  public ChargeFlywheel(Shooter shooterArg) {
+    shooter = shooterArg;
+    addRequirements(shooter);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    if (shooter.flyweelState == false) {
+      shooter.flyweelOn();
+    }
+    else if (shooter.flyweelState) {
+      System.out.println("Cant charge flywheel twice at once!");
+    }
+  } 
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+  }
 
   // Called once the command ends or is interrupted.
   @Override
@@ -27,6 +38,9 @@ public class ChargeFlywheel extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
-  }
-}
+    if(shooter.getFlywheelRPM() == Constants.FLYWHEEL_TAR_SPEED) {
+      return true;
+    } else {
+      return false;
+    }
+}}
