b8a98386c592b4d0e4f7b4c4539d7575df69bf13
Roman-Tech-Plus <66585473+Roman-Tech-Plus@users.noreply.github.com>
2022-02-03 22:31:28 -0800

    renamed folders

--- src/main/java/frc/robot/commands/AutoPaths/OffTarmack_H1.java
+++ src/main/java/frc/robot/commands/AutoPaths/OffTarmack_H1.java
@@ -2,11 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.AutoPaths;
+package frc.robot.commands.autoPaths;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.AutoActionCommands.MoveStraight;
-import frc.robot.commands.AutoActionCommands.Pivot;
+import frc.robot.commands.autoActionCommands.MoveStraight;
+import frc.robot.commands.autoActionCommands.Pivot;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Devices.IMU;
 
