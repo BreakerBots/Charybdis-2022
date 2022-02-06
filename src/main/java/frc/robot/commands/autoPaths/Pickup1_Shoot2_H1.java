25831b7f38a3866336b61dbacaeb4e92fa6c51de
Roman-Tech-Plus <66585473+Roman-Tech-Plus@users.noreply.github.com>
2022-02-05 16:09:16 -0800

    auto stuff

---
+++ src/main/java/frc/robot/commands/autoPaths/Pickup1_Shoot2_H1.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autoPaths;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.autoActionCommands.MoveStraight;
+import frc.robot.commands.autoActionCommands.Pivot;
+import frc.robot.commands.intakeCommands.IntakeToggle;
+import frc.robot.commands.shooterCommands.ShootAll;
+import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.Hopper;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Devices.IMU;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class Pickup1_Shoot2_H1 extends SequentialCommandGroup {
+  /** Creates a new Pickup1_Shoot2_H1. */
+  public Pickup1_Shoot2_H1(Drive driveArg, IMU imuArg, Intake intakeArg, Hopper hopperArg) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+      new MoveStraight(driveArg, 60, 0.7),
+      new Pivot(driveArg, imuArg, 5, 0.2),
+      new MoveStraight(driveArg, 96, 0.5),
+      new Pivot(driveArg, imuArg, 180, 0.5),
+      new IntakeToggle(intakeArg, hopperArg),
+      new MoveStraight(driveArg, 45, 0.7),
+      new Pivot(driveArg, imuArg, 10, 0.5)
+    );
+  }
+}
