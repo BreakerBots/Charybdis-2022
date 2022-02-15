6ca336f645602d6ede8c8f849de5b5f79260369f
Roman-Tech-Plus <66585473+Roman-Tech-Plus@users.noreply.github.com>
2022-02-14 22:04:01 -0800

    Update SmartDashboardControle.java

--- src/main/java/frc/robot/subsystems/SmartDashboardControle.java
+++ src/main/java/frc/robot/subsystems/SmartDashboardControle.java
@@ -4,6 +4,12 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.MjpegServer;
+import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.cscore.VideoMode.PixelFormat;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -13,6 +19,7 @@ public class SmartDashboardControle extends SubsystemBase {
   Intake intake;
   Climber climber;
   private double[] climbProg = new double[1];
+  
   public SmartDashboardControle(Shooter shooterArg, Intake intakeArg, Climber climbArg) {
     intake = intakeArg;
     shooter = shooterArg;
@@ -22,6 +29,7 @@ public class SmartDashboardControle extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.updateValues();
     switch (shooter.shooterMode) {
       case UP: SmartDashboard.putString("SHOOTER MODE: ", "HIGH");
       break;
@@ -35,6 +43,13 @@ public class SmartDashboardControle extends SubsystemBase {
     climbProg[1] = climber.climbSequenceProgress;
     SmartDashboard.putBoolean("INTAKEING: ", intake.intakeState);
     SmartDashboard.putNumberArray("CLIMB PROGRESS - A of B:", climbProg);
-    
+    UsbCamera usbCamera = new UsbCamera("USB Camera 0", 0);
+        CameraServer.startAutomaticCapture(usbCamera);
+
+        // Creates the CvSink and connects it to the UsbCamera
+        CvSink cvSink = CameraServer.getVideo();
+        
+        // Creates the CvSource and MjpegServer [2] and connects them
+        CvSource outputStream = CameraServer.putVideo("Blur", 640, 480);
   }
 }
