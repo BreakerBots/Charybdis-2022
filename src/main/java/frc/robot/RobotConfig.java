ca5679c131f4bf2e52972c5714389a1750573bae
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2022-02-21 14:29:04 -0800

    Beginning to add in RobotConfig

---
+++ src/main/java/frc/robot/RobotConfig.java
@@ -0,0 +1,18 @@
+package frc.robot;
+
+/**
+ * Planning to use this class to store a list of constants we can use to
+ * activate/deactivate any given subsystem.
+ */
+public class RobotConfig {
+
+    public static final boolean useIMU = true;
+    public static final boolean usePDP = true;
+    public static final boolean useCompressor = true;
+
+    public static final boolean useDrive = true;
+    public static final boolean useIntake = true;
+    public static final boolean useHopper = true;
+    public static final boolean useShooter = true;
+    public static final boolean useClimb = true;
+}
