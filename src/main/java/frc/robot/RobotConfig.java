13f35c7474e62572c247f9442bb024ed1926533b
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2022-02-22 14:16:22 -0800

    Added method for changing brake mode on motors

--- src/main/java/frc/robot/RobotConfig.java
+++ src/main/java/frc/robot/RobotConfig.java
@@ -1,5 +1,8 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
+
 /**
  * Planning to use this class to store a list of constants we can use to
  * activate/deactivate any given subsystem.
@@ -15,4 +18,14 @@ public class RobotConfig {
     public static final boolean useHopper = true;
     public static final boolean useShooter = true;
     public static final boolean useClimb = true;
+
+    public static void setBrakeMode(boolean modeArg, BaseMotorController... motors) {
+        for (int i = 0; i < motors.length; i++) {
+            if (modeArg) {
+                motors[i].setNeutralMode(NeutralMode.Brake);
+            } else {
+                motors[i].setNeutralMode(NeutralMode.Coast);
+            }
+        }
+    }
 }
