8a54973b10aec6a12ce4cf4c67df77d07ea4d48a
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2022-03-08 13:51:53 -0800

    Removing unused imports

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -12,9 +12,7 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.BreakerMath;
 import frc.robot.Constants;
 import frc.robot.RobotConfig;
 
