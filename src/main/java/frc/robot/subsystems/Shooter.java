e14c42abb56bada6e74a2b562b819e277bedac4d
Roman-Tech-Plus <66585473+Roman-Tech-Plus@users.noreply.github.com>
2022-03-10 20:30:14 -0800

    reverted shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -84,7 +84,7 @@ public class Shooter extends SubsystemBase {
   /** Makes flywheel charge to desired speed. */
   public void runFlywheel() {
     double flySpd = (flywheelPID.calculate(getFlywheelTPS(), getFlywheelTargetSpeed()));
-    flywheel.set(1);
+    flywheel.set(flyTgtSpdPrct);
     // double flydiff = getFlywheelTargetSpeed() - getFlywheelTPS();
     // double motorImpt = prevImpt + (flydiff * 0.0000055);
     // flywheel.set(motorImpt);
