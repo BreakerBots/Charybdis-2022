745ff1e26923e024d2673f5fd5b76dcb39af4f8f
YousifAlkhalaf <79383729+BreakerBots5104@users.noreply.github.com>
2022-03-16 15:52:27 -0700

    Create BreakerColorSensor.java

---
+++ src/main/java/frc/robot/BreakerLib/Devices/BreakerColorSensor.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.BreakerLib.Devices;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class BreakerColorSensor extends SubsystemBase {
+  /** Creates a new BreakerColorSensor. */
+  public BreakerColorSensor() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
